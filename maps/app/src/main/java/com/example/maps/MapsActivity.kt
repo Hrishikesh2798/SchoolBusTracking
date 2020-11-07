b3389f9fd2e07f4bafb6dd0bc4d21155e0e7065d
charan kumar reddy Guggulla <charankumar.g5@gmail.com>
2020-11-07 10:00:23 +0530

    Add files via upload

---
+++ maps/app/src/main/java/com/example/maps/MapsActivity.kt
@@ -0,0 +1,26 @@
+package com.example.maps
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val myCurrentLocation = LatLng(13.0827, 80.2707)
+        mMap.addMarker(MarkerOptions().position(myCurrentLocation).title("This is My current          Location"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myCurrentLocation))
+    }
+}
